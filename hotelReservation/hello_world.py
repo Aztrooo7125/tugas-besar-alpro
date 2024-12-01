b31bc4781affbc3582340baa8adba84fa3f26b98
Aztrooo7125 <viraldinarendraaztrooo@gmail.com>
2024-12-01 13:18:32 +0700

    made the placeholder file in hotelReservation more interesting.

--- hotelReservation/hello_world.py
+++ hotelReservation/hello_world.py
@@ -3,26 +3,72 @@ from time import sleep
 
 
 def main():
-    cascadeprint(input("Type Anything! "), 0.05)
+    cascade_print(input("Type Anything! "), 0.05)
 
 
-def cascadeprint(text, seconds):
-    sentence = list()
+def cascade_print(text: str, seconds: float) -> None:
+    """
+    Procedure to output text to terminal in a cascading fashion.
+
+    Args:
+        text (str)      : The text to be outputted cascadingly.
+        seconds (float) : The amount of delay between the printing
+                          of each character in text.
+
+    Returns:
+        None
+    """
+    sentence = []
     for char in range(len(text)): # char is returned as index (0,1,2,3, ...)
         current_char = text[char]
         if current_char.isupper():
-            checker(string.ascii_uppercase, sentence, seconds, current_char, char)
+            checker(
+                 string.ascii_uppercase, 
+                 sentence, 
+                 seconds, 
+                 current_char, 
+                 char
+            )
         elif current_char.islower():
-            checker(string.ascii_lowercase, sentence, seconds, current_char, char)
+            checker(
+                 string.ascii_lowercase, 
+                 sentence, 
+                 seconds, 
+                 current_char, 
+                 char
+            )
         elif current_char in string.punctuation:
-            checker(string.punctuation, sentence, seconds, current_char, char)
+            checker(
+                 string.punctuation, 
+                 sentence, 
+                 seconds, 
+                 current_char, 
+                 char
+            )
         elif current_char.isdigit():
-            checker(string.digits, sentence, seconds, current_char, char)
+            checker(
+                 string.digits, 
+                 sentence, 
+                 seconds, 
+                 current_char, 
+                 char
+            )
         elif current_char.isspace():
                     sentence.append(" ")
 
 
-def checker(stringtype, sen, delay, current_letter, i):
+def checker(stringtype, sen: str, delay: float, current_letter: str, idx: int) -> None:
+    """
+    Args:
+        stringtype          : The type of string, whether it's alphabet, digits, punctuation, etc.
+        sen (str)           : The string it is checking currently.
+        delay (float)       : The amount of delay between the printing of each character in text.
+        current_letter (str): The letter this procedure is currently checking.
+        idx (int)           : Index of the string.
+
+    Returns:
+        None
+    """
     _ = True
     for c in stringtype:
         if _:
@@ -30,12 +76,14 @@ def checker(stringtype, sen, delay, current_letter, i):
             print(*sen, sep="")
             sleep(delay)
             _ = False
+
         else:
-            sen[i] = c 
+            sen[idx] = c 
             print(*sen, sep="")
             sleep(delay)
+
         if c == current_letter:
-            break
+            return
 
 
 if __name__ == "__main__":
